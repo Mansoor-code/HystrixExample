2dd285cac3fc4bc2fb2cb5e18810f2844fb8bfd9
mansoor.sajjad <sh.mansoor.com@gmail.com>
2020-11-21 12:13:06 +0100

    Hystrix Example

---
+++ BookStoreReader/src/test/java/com/spring/bookstore/reader/BookStoreReader/BookStoreReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.bookstore.reader.BookStoreReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
